e6cf35a9cc82ce6ee980d3f8bb822e6fbb7c0c99
Rahul <rahulparaselli8074@gamil.com>
2024-09-09 11:07:52 +0530

    Changes until the serch for tweets 2

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 import time
 import pickle
+from lxml import etree
 
 # Create a new instance of the Firefox driver
 driver = webdriver.Firefox()
@@ -29,9 +31,29 @@ time.sleep(10)
 
 # search for tweets
 driver.get("https://x.com/explore")
-searchBox = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div/div/div/form/div[1]/div/div/div/div/div[2]/div/input")
+searchBox = driver.find_element(By.CLASS_NAME, "r-30o5oe")
 searchBox.click()
 searchBox.send_keys("#floods #India")
 time.sleep(2)
 searchBox.send_keys(Keys.RETURN)
 
+"""soup = BeautifulSoup(driver.page_source, 'lxml')
+pickle.dump(soup, open("tweets.pkl", "wb"))
+
+#/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[3]/section/div/div/div[1]/div/div/article
+soup = pickle.load(open("tweets.pkl", "rb"))
+soup = soup.find_all("div")
+for div in soup:
+    print(div.text)
+    time.sleep(10)"""
+
+# scroll down
+articles_list = []
+for i in range(10):
+    artilces = driver.find_elements(By.TAG_NAME, "article")
+    for article in artilces:
+        articles_list.append(article.text)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(3)
+
+print(articles_list)
